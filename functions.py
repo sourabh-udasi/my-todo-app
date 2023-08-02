1c5db766602d3bb416e27d31c1c1bc7a1e97807e
sourabh-udasi <sourabhudasi1995@gmail.com>
2023-08-02 15:58:20 +0530

    initia commit

---
+++ functions.py
@@ -0,0 +1,20 @@
+Filepath = 'todo.txt'
+
+
+def get_todos(filepath=Filepath):
+    """ Read a text fle and return the list of
+    to do items
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=Filepath):
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+    print("Hello")
+    print(get_todos())
---
+++ web.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import functions
+import os
+
+if not os.path.exists("todo.txt"):
+    with open("todo.txt", "w") as file:
+        pass
+
+todos = functions.get_todos()
+
+
+def add_todo():
+    todo = st.session_state['new_todo'] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
+st.title("My Todo App")
+st.subheader("This is my todo app.")
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
+st.text_input(label=" ", placeholder="Add New Todo.....",
+              on_change=add_todo, key='new_todo')
+
+# x = len(st.session_state['new_todo'])
+# if x == 0:
